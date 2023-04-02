3822d75376fdf5d869b640f3dd935c3b147d618b
vardhanreddy27 <63119222+vardhanreddy27@users.noreply.github.com>
2023-04-02 10:20:52 +0530

    Vardhan (#6)
    * push to branch
    
    * initial commit2
    
    * menu without navigation added
    
    * menu  navigation added
    
    * shoe card added
    
    * check git pull

--- sair/src/components/HeroBanner.tsx
+++ sair/src/components/HeroBanner.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function HeroBanner() {
-  return <div className="hero p-5 stick ">home content</div>;
+  return <div className="hero p-5 stick ">home content here..</div>;
 }
 
 export default HeroBanner;

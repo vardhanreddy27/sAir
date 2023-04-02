f196764de93d2b41f64e6f1bfc8ea02107f04017
12201412-zubair <113511809+zubair647@users.noreply.github.com>
2023-04-03 00:02:14 +0530

    Zubair (#12)
    * Dev (#7)
    
    * Vardhan (#2)
    
    * push to branch
    
    * initial commit2
    
    * menu without navigation added
    
    * Vardhan (#3)
    
    * push to branch
    
    * initial commit2
    
    * menu without navigation added
    
    * Vardhan (#4)
    
    * push to branch
    
    * initial commit2
    
    * menu without navigation added
    
    * menu  navigation added
    
    * Vardhan (#5)
    
    * push to branch
    
    * initial commit2
    
    * menu without navigation added
    
    * menu  navigation added
    
    * shoe card added
    
    * Vardhan (#6)
    
    * push to branch
    
    * initial commit2
    
    * menu without navigation added
    
    * menu  navigation added
    
    * shoe card added
    
    * check git pull
    
    * Dev (#10)
    
    * Vardhan (#2)
    
    * push to branch
    
    * initial commit2
    
    * menu without navigation added
    
    * Vardhan (#3)
    
    * push to branch
    
    * initial commit2
    
    * menu without navigation added
    
    * Vardhan (#4)
    
    * push to branch
    
    * initial commit2
    
    * menu without navigation added
    
    * menu  navigation added
    
    * Vardhan (#5)
    
    * push to branch
    
    * initial commit2
    
    * menu without navigation added
    
    * menu  navigation added
    
    * shoe card added
    
    * Vardhan (#6)
    
    * push to branch
    
    * initial commit2
    
    * menu without navigation added
    
    * menu  navigation added
    
    * shoe card added
    
    * check git pull
    
    * Vardhan (#8)
    
    * check git pull
    
    * Dev (#7)
    
    * Vardhan (#2)
    
    * push to branch
    
    * initial commit2
    
    * menu without navigation added
    
    * Vardhan (#3)
    
    * push to branch
    
    * initial commit2
    
    * menu without navigation added
    
    * Vardhan (#4)
    
    * push to branch
    
    * initial commit2
    
    * menu without navigation added
    
    * menu  navigation added
    
    * Vardhan (#5)
    
    * push to branch
    
    * initial commit2
    
    * menu without navigation added
    
    * menu  navigation added
    
    * shoe card added
    
    * Vardhan (#6)
    
    * push to branch
    
    * initial commit2
    
    * menu without navigation added
    
    * menu  navigation added
    
    * shoe card added
    
    * check git pull
    
    * check git pulls 2
    
    * Vardhan (#9)
    
    * check git pull
    
    * Dev (#7)
    
    * Vardhan (#2)
    
    * push to branch
    
    * initial commit2
    
    * menu without navigation added
    
    * Vardhan (#3)
    
    * push to branch
    
    * initial commit2
    
    * menu without navigation added
    
    * Vardhan (#4)
    
    * push to branch
    
    * initial commit2
    
    * menu without navigation added
    
    * menu  navigation added
    
    * Vardhan (#5)
    
    * push to branch
    
    * initial commit2
    
    * menu without navigation added
    
    * menu  navigation added
    
    * shoe card added
    
    * Vardhan (#6)
    
    * push to branch
    
    * initial commit2
    
    * menu without navigation added
    
    * menu  navigation added
    
    * shoe card added
    
    * check git pull
    
    * check git pulls 2
    
    * add cmts here wfef
    
    * add cmts these
    
    * changed here
    
    ---------
    
    Co-authored-by: vardhanreddy27 <63119222+vardhanreddy27@users.noreply.github.com>
    Co-authored-by: zubair ahmed <zubairahmed@zubairs-MacBook-Air.local>

--- sair/src/components/Navbar.tsx
+++ sair/src/components/Navbar.tsx
@@ -1,27 +1,7 @@
 import React from "react";
 function Navbar() {
   return <div className=" row navbar">
-
-
-
-
-
-
-<ul>
-  <li>
-    home
-  </li>
-  <li>
-   mdon
-  </li>
-  <li>
-   rwfkinwvio
-  </li>
-</ul>
-
-
-
-  </div>;
+Navbar  </div>;
 }
 
 export default Navbar;

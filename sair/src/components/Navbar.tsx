150a96a0890539c960197f06b86bb2d4c249dde2
zubair ahmed <zubairahmed@zubairs-MacBook-Air.local>
2023-04-03 21:54:24 +0530

    added nav

--- sair/src/components/Navbar.tsx
+++ sair/src/components/Navbar.tsx
@@ -8,12 +8,13 @@ function Navbar() {
     <img src="logo512.png " width="30" height="30" className="d-inline-block align-top " alt="" />
     <a className="logo-text ">sAir</a>
   </a>
-
-  <button className=" buttom-nav btn btn-outline-success my-2 my-sm-0 " type="submit">Search</button>
+  
+  
 </nav>
   
   
   
-  </div>;
+  </div>
+  
 }
 export default Navbar;

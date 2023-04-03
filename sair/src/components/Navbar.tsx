9fa35983b902c1a4af2a8f625e7bc49f869e8aff
zubair ahmed <zubairahmed@zubairs-MacBook-Air.local>
2023-04-03 21:40:32 +0530

    added logo and search button as of now

--- sair/src/components/Navbar.tsx
+++ sair/src/components/Navbar.tsx
@@ -1,6 +1,19 @@
 import React from "react";
 function Navbar() {
-  return <div className=" row navbar">logo here</div>;
-}
+  return <div className=" row navbar">
+    
+<nav className="navbar navbar-light bg-light">
+  <a className="navbar-brand nav_img" href="#">
+    
+    <img src="logo512.png " width="30" height="30" className="d-inline-block align-top " alt="" />
+    <a className="logo-text ">sAir</a>
+  </a>
 
+  <button className=" buttom-nav btn btn-outline-success my-2 my-sm-0 " type="submit">Search</button>
+</nav>
+  
+  
+  
+  </div>;
+}
 export default Navbar;

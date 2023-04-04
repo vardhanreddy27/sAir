5f429e80192930eb6e5e09deea4d2a14cf09287c
12201412-zubair <113511809+zubair647@users.noreply.github.com>
2023-04-04 22:44:19 +0530

    Zubair (#15)
    * added logo and search button as of now
    
    * added nav
    
    ---------
    
    Co-authored-by: zubair ahmed <zubairahmed@zubairs-MacBook-Air.local>

--- sair/src/index.tsx
+++ sair/src/index.tsx
@@ -7,7 +7,6 @@ import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.js";
-import Popper from "popper.js";
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 root.render(

7ab0879694e1ce131e2cca1de16431209c3771de
Khan <mustafeez72777@gmail.com>
2023-04-14 13:58:36 +0530

    updated herobanner

--- sair/src/components/HeroBanner.tsx
+++ sair/src/components/HeroBanner.tsx
@@ -18,9 +18,147 @@ function HeroBanner() {
       </div>
 
 
+      <div className="items-name-section">
+        <div className="d-flex flex-row">
+          <div className="item-name">
+            <h5>All items</h5>
+          </div>
+
+          <div className="item-name">
+            <h5>Shoes</h5>
+          </div>
+
+          <div className="item-name">
+            <h5>Accessories</h5>
+          </div>
+
+          <div className="item-name">
+            <h5>Electronics</h5>
+          </div>
+
+          <div className="item-name">
+            <h5>Clothing</h5>
+          </div>
+
+          <div className="item-name">
+            <h5>Kid's</h5>
+          </div>
+
+          <div className="item-name">
+            <h5>Sports</h5>
+          </div>
+
+          <div className="item-name">
+            <h5>Beauty</h5>
+          </div>
+
+          <div className="item-name">
+            <h5>Fashion</h5>
+          </div>
+
+        </div>
+      </div>
+
+
+      <div className="circleitems -section">
+        <div className="d-flex flex-row">
+          <div className="image-product-section">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRS-VENNBLjW1zBQEj6Esb2mY4XgI2mJX-byvZlbihvplSDp8yXEVLaBCLbNpI0ChkA5oc&usqp=CAU" 
+            alt="" className="image-card shadow" />
+            <div className="image-heading">
+              <h4>Sun glasses</h4>
+              <p>$29</p>
+            </div>
+          </div>
+
+          <div>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSclp5g9V0peyQ-9-6uPbAyUrtj0mgyOm-ZA&usqp=CAU" 
+            alt="" className="image-card shadow" />
+            <div className="image-heading">
+              <h4>Shoes</h4>
+              <p>$229</p>
+            </div>
+          </div>
+
+          <div>
+            <img src="https://images.unsplash.com/photo-1559563458-527698bf5295?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aGFuZGJhZ3xlbnwwfHwwfHw%3D&w=1000&q=80" 
+            alt="" className="image-card shadow" />
+            <div className="image-heading">
+              <h4>Hand bag</h4>
+              <p>$149,76</p>
+            </div>
+          </div>
+
+          <div>
+            <img src="https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/452163/item/goods_01_452163.jpg?width=750"  
+            alt="" className="image-card shadow" />
+            <div className="image-heading">
+              <h4>Men's Fluffy</h4>
+              <p>$99,95</p>
+            </div>
+          </div>
+
+          <div>
+            <img src="https://ae01.alicdn.com/kf/HTB162hkbe6sK1RjSsrbq6xbDXXaq/Spring-men-oversize-jacket-fashion-Couple-Solid-color-thin-loose-hooded-coat-autumn-mens-Single-breasted.jpg_Q90.jpg_.webp" 
+            alt="" className="image-card shadow" />
+            <div className="image-heading">
+              <h4>Black hoodie</h4>
+              <p>$29</p>
+            </div>
+          </div>
+
+          <div>
+            <img src="https://images.unsplash.com/photo-1559563458-527698bf5295?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aGFuZGJhZ3xlbnwwfHwwfHw%3D&w=1000&q=80" 
+            alt="" className="image-card shadow" />
+            <div className="image-heading">
+              <h4>Hand bag</h4>
+              <p>$149,76</p>
+            </div>
+          </div>
+
+          <div>
+            <img src="https://images.unsplash.com/photo-1559563458-527698bf5295?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aGFuZGJhZ3xlbnwwfHwwfHw%3D&w=1000&q=80" 
+            alt="" className="image-card shadow" />
+            <div className="image-heading">
+              <h4>Hand bag</h4>
+              <p>$149,76</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+    <div className="offer-carousel-section shadow">
+      <div>
+        <div id="carouselExampleInterval" className="carousel slide" data-bs-ride="carousel">
+          <div className="carousel-inner">
+            <div className="carousel-item active" data-bs-interval="10000">
+              <img src="https://s3b.cashify.in/gpro/uploads/2022/09/24160037/Homepage-Festive_Offer_banner_450x225.png" className="d-block w-100" alt="..." />
+            </div>
+            <div className="carousel-item" data-bs-interval="2000">
+              <img src="https://s3b.cashify.in/gpro/uploads/2022/09/24160037/Homepage-Festive_Offer_banner_450x225.png" className="d-block w-100" alt="..." />
+            </div>
+            <div className="carousel-item">
+               <img src="https://s3b.cashify.in/gpro/uploads/2022/09/24160037/Homepage-Festive_Offer_banner_450x225.png" className="d-block w-100" alt="..." />
+             </div>
+          </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+        </div>
+      </div>
+    </div>
+
 
       <div className="shop-bottom-section">
-<div className="d-flex flex-row ">
+        <div className="d-flex flex-row ">
 
         <div className="shop-card-item shadow">
           <img

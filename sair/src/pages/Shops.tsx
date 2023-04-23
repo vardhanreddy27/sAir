ae62b24177ea9b4e8b9be514aca95389e592c5e2
vardhanreddy27 <63119222+vardhanreddy27@users.noreply.github.com>
2023-04-24 00:15:40 +0530

    Zubair (#42) (#43)
    * final css fix
    
    * signup login fix
    
    * final css fix
    
    * home,support,navbar  updated
    
    * track orders
    
    * added wishlist
    
    * location updated
    
    * location updates
    
    * Dev (#37)
    
    * Vardhan (#30)
    
    * final css fix
    
    * signup login fix
    
    * final css fix
    
    * home,support,navbar  updated
    
    * Vardhan (#32)
    
    * final css fix
    
    * signup login fix
    
    * final css fix
    
    * home,support,navbar  updated
    
    * track orders
    
    * Vardhan (#34)
    
    * final css fix
    
    * signup login fix
    
    * final css fix
    
    * home,support,navbar  updated
    
    * track orders
    
    * added wishlist
    
    * Vardhan (#36)
    
    * final css fix
    
    * signup login fix
    
    * final css fix
    
    * home,support,navbar  updated
    
    * track orders
    
    * added wishlist
    
    * location updated
    
    * location updates
    
    * dropdown for loc added
    
    * Vardhan (#39) (#40)
    
    * final css fix
    
    * signup login fix
    
    * final css fix
    
    * home,support,navbar  updated
    
    * track orders
    
    * added wishlist
    
    * location updated
    
    * location updates
    
    * Dev (#37)
    
    * Vardhan (#30)
    
    * final css fix
    
    * signup login fix
    
    * final css fix
    
    * home,support,navbar  updated
    
    * Vardhan (#32)
    
    * final css fix
    
    * signup login fix
    
    * final css fix
    
    * home,support,navbar  updated
    
    * track orders
    
    * Vardhan (#34)
    
    * final css fix
    
    * signup login fix
    
    * final css fix
    
    * home,support,navbar  updated
    
    * track orders
    
    * added wishlist
    
    * Vardhan (#36)
    
    * final css fix
    
    * signup login fix
    
    * final css fix
    
    * home,support,navbar  updated
    
    * track orders
    
    * added wishlist
    
    * location updated
    
    * location updates
    
    * dropdown for loc added
    
    * stores
    
    ---------
    
    Co-authored-by: zubair ahmed <zubairahmed@zubairs-MacBook-Air.local>

--- sair/src/pages/Shops.tsx
+++ sair/src/pages/Shops.tsx
@@ -12,15 +12,18 @@ function Shops() {
 
 
 
+      <div className="shops-name-section">
+        <h4>Electronics</h4>
       <div className="col d-flex flex-row">
-        <div className=" shops-section shadow">
-          <div className="d-flex flex-row">
+        <div className="shops-section shadow">
+          <div className=" d-flex flex-row">
             <div>
-              <img src="salad.png"  alt="" className="rounded-circle icon-shape icon-x1" />
+              <img src="electronics.png"  alt="" className="shops-icons-sec icon-shape icon-x1" />
             </div>
             <div className="shops-text-card">
-              <h4>E-Grocery Super Market</h4>
+              <h6>Reliance digital</h6>
               <p>GT road Jalandhar</p>
+              <p>4.5⭐</p>
               <div>
                 <div className="badge text-bg-light border"><span className="text-primary">Delivery in 49 min</span></div>
               </div>
@@ -31,11 +34,12 @@ function Shops() {
         <div className=" shops-section shadow">
           <div className="d-flex flex-row">
             <div>
-              <img src="online-payment.png"  alt="" className="rounded-circle icon-shape icon-x1" />
+              <img src="electric-appliance.png"  alt="" className="shops-icons-sec icon-shape icon-x1" />
             </div>
             <div className="shops-text-card">
-              <h4>Life Style</h4>
+              <h6>Bajaj Electronics</h6>
               <p>Trunk road phagwara</p>
+              <p>4.3⭐</p>
               <div>
                 <div className="badge text-bg-light border"><span className="text-primary">Delivery in 25 min</span></div>
               </div>
@@ -44,14 +48,15 @@ function Shops() {
         </div>
 
 
-        <div className=" shops-section shadow">
+        <div className="shops-section shadow">
           <div className="d-flex flex-row">
             <div>
-              <img src="grocery-store.png"  alt="" className="rounded-circle icon-shape icon-x1" />
+              <img src="gadgets.png"  alt="" className="shops-icons-sec icon-shape icon-x1" />
             </div>
             <div className="shops-text-card">
-              <h4>DMart</h4>
-              <p>Trunk road phagwara</p>
+              <h6>Sonovision</h6>
+              <p>Rajiv Park</p>
+              <p>4.1⭐</p>
               <div>
                 <div className="badge text-bg-light border"><span className="text-primary">Delivery in 45 min</span></div>
               </div>
@@ -61,16 +66,18 @@ function Shops() {
       </div>
 
 
-
+      
       <div className="col d-flex flex-row">
         <div className=" shops-section shadow">
           <div className="d-flex flex-row">
             <div>
-              <img src="grocery-store.png"  alt="" className="rounded-circle icon-shape icon-x1" />
+              <img src="grocery-store.png"  alt="" className="shops-icons-sec icon-shape icon-x1" />
             </div>
             <div className="shops-text-card">
-              <h4>Spencers</h4>
-              <p>Trunk road phagwara</p>
+              <h6>Big C
+              </h6>
+              <p>New Bus stand</p>
+              <p>3.8⭐</p>
               <div>
                 <div className="badge text-bg-light border"><span className="text-primary">Delivery in 30 min</span></div>
               </div>
@@ -81,11 +88,12 @@ function Shops() {
         <div className=" shops-section shadow">
           <div className="d-flex flex-row">
             <div>
-              <img src="boutique.png"  alt="" className="rounded-circle icon-shape icon-x1" />
+              <img src="store.png"  alt="" className="shops-icons-sec" />
             </div>
             <div className="shops-text-card">
-              <h4>Reliance</h4>
-              <p>Trunk road phagwara</p>
+              <h6>A-one  Mobiles</h6>
+              <p>Opp to Blind School</p>
+              <p>3.6⭐</p>
               <div>
                 <div className="badge text-bg-light border"><span className="text-primary">Delivery in 55 min</span></div>
               </div>
@@ -97,11 +105,128 @@ function Shops() {
         <div className=" shops-section shadow">
           <div className="d-flex flex-row">
             <div>
-              <img src="online-payment.png"  alt="" className="rounded-circle icon-shape icon-x1" />
+              <img src="online-payment.png"  alt="" className="shops-icons-sec icon-shape icon-x1" />
             </div>
             <div className="shops-text-card">
-              <h4>Trends</h4>
+              <h6>Trends</h6>
               <p>Trunk road phagwara</p>
+              <p>3.5⭐</p>
+              <div>
+                <div className="badge text-bg-light border"><span className="text-primary">Delivery in 31 min</span></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+
+
+      <div>
+        <h4>Super Markets</h4>
+      <div className="col d-flex flex-row ">
+        <div className=" shops-section shadow">
+          <div className="d-flex flex-row">
+            <div>
+              <img src="salad.png"  alt="" className="shops-icons-sec icon-shape icon-x1" />
+            </div>
+            <div className="shops-text-card">
+              <h6>E-Grocery Super Market</h6>
+              <p>GT road Jalandhar</p>
+              <p>4.6⭐</p>
+              <div>
+                <div className="badge text-bg-light border"><span className="text-primary">Delivery in 49 min</span></div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className=" shops-section shadow">
+          <div className="d-flex flex-row">
+            <div>
+              <img src="grocery-cart.png"  alt="" className="shops-icons-sec icon-shape icon-x1" />
+            </div>
+            <div className="shops-text-card">
+              <h6>Spencers</h6>
+              <p>Opp to Bus Stand </p>
+              <p>4.2⭐</p>
+              <div>
+                <div className="badge text-bg-light border"><span className="text-primary">Delivery in 25 min</span></div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className=" shops-section shadow">
+          <div className="d-flex flex-row">
+            <div>
+              <img src="supermarket.png"  alt="" className="shops-icons-sec icon-shape icon-x1" />
+            </div>
+            <div className="shops-text-card">
+              <h6>DMart</h6>
+              <p>Trunk road phagwara</p>
+              <p>4.0⭐</p>
+              <div>
+                <div className="badge text-bg-light border"><span className="text-primary">Delivery in 45 min</span></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+
+
+      <div className="col d-flex flex-row ">
+        <div className=" shops-section shadow">
+          <div className="d-flex flex-row">
+            <div>
+              <img src="basket.png"  alt="" className="shops-icons-sec icon-shape icon-x1" />
+            </div>
+            <div className="shops-text-card">
+              <h6>More</h6>
+              <p>NGO Colony</p>
+              <p>3.9⭐</p>
+              <div>
+                <div className="badge text-bg-light border"><span className="text-primary">Delivery in 30 min</span></div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className=" shops-section shadow">
+          <div className="d-flex flex-row">
+            <div>
+              <img src="shopping-bag.png"  alt="" className="shops-icons-sec icon-shape icon-x1" />
+            </div>
+            <div className="shops-text-card">
+              <h6>Super K</h6>
+              <p>Apsara circle</p>
+              <p>3.7⭐</p>
+              <div>
+                <div className="badge text-bg-light border"><span className="text-primary">Delivery in 55 min</span></div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className=" shops-section shadow">
+          <div className="d-flex flex-row">
+            <div>
+              <img src="online-payment.png"  alt="" className="shops-icons-sec icon-shape icon-x1" />
+            </div>
+            <div className="shops-text-card">
+              <h6>Bazaar</h6>
+              <p>YV street</p>
+              <p>3.3⭐</p>
               <div>
                 <div className="badge text-bg-light border"><span className="text-primary">Delivery in 31 min</span></div>
               </div>
@@ -111,19 +236,138 @@ function Shops() {
       </div>
 
 
+
+
+
+
+      <div className="shops-name-section">
+        <h4>Fashion&Beauty</h4>
+      <div className="col d-flex flex-row ">
+        <div className=" shops-section shadow">
+          <div className="d-flex flex-row">
+            <div>
+              <img src="beauty-salon.png"  alt="" className="shops-icons-sec icon-shape icon-x1" />
+            </div>
+            <div className="shops-text-card">
+              <h6>Vikas Fashions</h6>
+              <p>Madras road</p>
+              <p>4.8⭐</p>
+              <div>
+                <div className="badge text-bg-light border"><span className="text-primary">Delivery in 49 min</span></div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className=" shops-section shadow">
+          <div className="d-flex flex-row">
+            <div>
+              <img src="consumption.png"  alt="" className="shops-icons-sec icon-shape icon-x1" />
+            </div>
+            <div className="shops-text-card">
+              <h6>Indian Fashion</h6>
+              <p>YV street</p>
+              <p>4.4⭐</p>
+              <div>
+                <div className="badge text-bg-light border"><span className="text-primary">Delivery in 25 min</span></div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className=" shops-section shadow">
+          <div className="d-flex flex-row">
+            <div>
+              <img src="dress.png"  alt="" className="shops-icons-sec icon-shape icon-x1" />
+            </div>
+            <div className="shops-text-card">
+              <h6>Life Style</h6>
+              <p>RSR Square Mall</p>
+              <p>4.1⭐</p>
+              <div>
+                <div className="badge text-bg-light border"><span className="text-primary">Delivery in 45 min</span></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </div>
+
+
+      <div className="col d-flex flex-row ">
+        <div className=" shops-section shadow">
+          <div className="d-flex flex-row">
+            <div>
+              <img src="boutique.png"  alt="" className="shops-icons-sec icon-shape icon-x1" />
+            </div>
+            <div className="shops-text-card">
+              <h6>Dress Circle</h6>
+              <p>Koti Reddy Circle</p>
+              <p>3.7⭐</p>
+              <div>
+                <div className="badge text-bg-light border"><span className="text-primary">Delivery in 30 min</span></div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className=" shops-section shadow">
+          <div className="d-flex flex-row">
+            <div>
+              <img src="boutique.png"  alt="" className="shops-icons-sec icon-shape icon-x1" />
+            </div>
+            <div className="shops-text-card">
+              <h6>Unlimited</h6>
+              <p>Opp KFC</p>
+              <p>3.2⭐</p>
+              <div>
+                <div className="badge text-bg-light border"><span className="text-primary">Delivery in 55 min</span></div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <div className=" shops-section shadow">
+          <div className="d-flex flex-row">
+            <div>
+              <img src="makeup-pouch.png"  alt="" className="shops-icons-sec icon-shape icon-x1" />
+            </div>
+            <div className="shops-text-card">
+              <h5>Lakhme Salon</h5>
+              <p>Beside DC</p>
+              <p>4.0⭐</p>
+              <div>
+                <div className="badge text-bg-light border"><span className="text-primary">Delivery in 31 min</span></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+
       <div className="recent-viewed-card">
         <h3>Recently Viewed</h3>
       </div>
 
-      <div className="col d-flex flex-row">
+      <div className="col d-flex flex-row ">
         <div className=" shops-section shadow">
           <div className="d-flex flex-row">
             <div>
-              <img src="boutique.png"  alt="" className="rounded-circle icon-shape icon-x1" />
+              <img src="boutique.png"  alt="" className="shops-icons-sec icon-shape icon-x1" />
             </div>
             <div className="shops-text-card">
-              <h4>H&M</h4>
-              <p>Trunk road phagwara</p>
+              <h6>Vishal Mart</h6>
+              <p>RS Road</p>
+              <p>3.8⭐</p>
               <div>
                 <div className="badge text-bg-light border"><span className="text-primary">Delivery in 50 min</span></div>
               </div>
@@ -134,11 +378,12 @@ function Shops() {
         <div className=" shops-section shadow">
           <div className="d-flex flex-row">
             <div>
-              <img src="fashion.png"  alt="" className="rounded-circle icon-shape icon-x1" />
+              <img src="fashion.png"  alt="" className="shops-icons-sec icon-shape icon-x1" />
             </div>
             <div className="shops-text-card">
-              <h4>Zudio</h4>
+              <h6>Zudio</h6>
               <p>Trunk road phagwara</p>
+              <p>3.3⭐</p>
               <div>
                 <div className="badge text-bg-light border"><span className="text-primary">Delivery in 50 min</span></div>
               </div>
@@ -150,11 +395,12 @@ function Shops() {
         <div className=" shops-section shadow">
           <div className="d-flex flex-row">
             <div>
-              <img src="grocery-store.png"  alt="" className="rounded-circle icon-shape icon-x1" />
+              <img src="grocery-store.png"  alt="" className="shops-icons-sec icon-shape icon-x1" />
             </div>
             <div className="shops-text-card">
-              <h4>Big Basket</h4>
+              <h6>Big Basket</h6>
               <p>Trunk road phagwara</p>
+              <p>4.0⭐</p>
               <div>
                 <div className="badge text-bg-light border"><span className="text-primary">Delivery in 23 min</span></div>
               </div>
@@ -163,6 +409,7 @@ function Shops() {
         </div>
       </div>
 
+      
       
 
     </div>

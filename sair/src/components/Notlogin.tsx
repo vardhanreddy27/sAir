eb902b5b3b2095d2acecde827d33372f5da6857b
vardhanreddy27 <2527webmaster@gmail.com>
2023-05-04 00:30:16 +0530

    added login

--- sair/src/components/Notlogin.tsx
+++ sair/src/components/Notlogin.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Notlogin() {
   return (
-    <div className='row  mt-5 bg-light p-5'>
-        <h5>You have not logged in </h5>
-        <p>Please login</p>
+    <div className='myco'>
+     <div className="row hfr text-center">
+      <img src='https://i.pinimg.com/originals/e5/07/d7/e507d704d4b6fdcb17116762fcd99acd.gif' className="gif" alt="truch" />
+   
+    <Link to="Login"> <button type="button" className="btn btn-primary mart text-center ">Login / Signup</button></Link>
+     <p className="p-1">You have not Logged in please login</p>
+
+    </div> 
+ 
     </div>
   )
 }
--- sair/src/pages/Login.tsx
+++ sair/src/pages/Login.tsx
@@ -1,7 +1,181 @@
-import React from "react";
+import React, { useState } from "react";
+import Navbar from "../components/Navbar";
+import { AiFillHome, AiOutlineStar, AiOutlineUserAdd } from "react-icons/ai";
+import { BiSupport } from "react-icons/bi";
+import { BsShop } from "react-icons/bs";
+import { SiOpenstreetmap } from "react-icons/si";
+import { Link } from "react-router-dom";
+import Signup from "./Signup";
 
 function Login() {
-  return <div className="p-5 stick mt-5">Login</div>;
+  const [isActive, setIsActive] = useState(false);
+  const [isSecond, setIsSecond] = useState(false);
+  const [isThree, setIsThree] = useState(false);
+  const [isFour, setIsFour] = useState(false);
+  const [isFive, setIsFive] = useState(false);
+  const [isSix, setIsSix] = useState(false);
+  const [isEight, setIsEight] = useState(true);
+  const handleClick = () => {
+    setIsActive(true);
+    setIsSecond(false);
+    setIsThree(false);
+    setIsFour(false);
+    setIsFive(false);
+    setIsSix(false);
+    setIsEight(false);
+  };
+  const handleClick1 = () => {
+    setIsActive(false);
+    setIsSecond(true);
+    setIsThree(false);
+    setIsFour(false);
+    setIsFive(false);
+    setIsSix(false);
+    setIsEight(false);
+  };
+  const handleClick2 = () => {
+    setIsActive(false);
+    setIsSecond(false);
+    setIsThree(true);
+    setIsFour(false);
+    setIsFive(false);
+    setIsSix(false);
+    setIsEight(false);
+  };
+  const handleClick3 = () => {
+    setIsActive(false);
+    setIsSecond(false);
+    setIsThree(false);
+    setIsFour(true);
+    setIsFive(false);
+    setIsSix(false);
+    setIsEight(false);
+  };
+  const handleClick4 = () => {
+    setIsActive(false);
+    setIsSecond(false);
+    setIsThree(false);
+    setIsFour(false);
+    setIsFive(true);
+    setIsSix(false);
+    setIsEight(false);
+  };
+  const handleClick5 = () => {
+    setIsActive(false);
+    setIsSecond(false);
+    setIsThree(false);
+    setIsFour(false);
+    setIsFive(false);
+    setIsSix(true);
+    setIsEight(false);
+  };
+  const handleClick6 = () => {
+    setIsActive(false);
+    setIsSecond(false);
+    setIsThree(false);
+    setIsFour(false);
+    setIsFive(false);
+    setIsSix(false);
+    setIsEight(false);
+  };
+  const handleClick7 = () => {
+    setIsActive(false);
+    setIsSecond(false);
+    setIsThree(false);
+    setIsFour(false);
+    setIsFive(false);
+    setIsSix(false);
+    setIsEight(true);
+  };
+  return(
+  <div className="fluid-container">
+  <Navbar />
+  <div className="row">
+    <div className="col-2 sticky  hidden-mobile">
+
+      <div className="menu">
+        <div className="d-grid mt-5">
+
+        <button
+            type="button"
+            className={
+              isActive
+                ? "btn3 btn btn-primary sidemenu myt"
+                : "btn3 btn sidemenu myt"
+            }
+            onClick={handleClick}
+          >
+            <Link to="/">
+              <AiFillHome className="menuicon" /> Home
+            </Link>
+          </button>
+                   <button
+            type="button"
+            className={
+              isThree
+                ? "btn3 btn btn-primary sidemenu"
+                : "btn3 btn sidemenu"
+            }
+            onClick={handleClick2}
+          >
+            <BsShop className="menuicon" />
+            Shops
+          </button>
+          <button
+            type="button"
+            className={
+              isFour
+                ? "btn3 btn btn-primary sidemenu"
+                : "btn3 btn sidemenu"
+            }
+            onClick={handleClick3}
+          >
+            <SiOpenstreetmap className="menuicon" /> Track orders
+          </button> 
+          <button
+            type="button"
+            className={
+              isFive
+                ? "btn3 btn btn-primary sidemenu"
+                : "btn3 btn sidemenu"
+            }
+            onClick={handleClick4}
+          >
+            <AiOutlineStar className="menuicon" /> Wishlist
+          </button>
+          <button
+            type="button"
+            className={
+              isSix
+                ? "btn3 btn btn-primary sidemenu"
+                : "btn3 btn sidemenu"
+            }
+            onClick={handleClick5}
+          >
+            <BiSupport className="menuicon" /> Support
+          </button>
+          <hr />
+          <div className="OfferCard">
+            <img src="./shoe.png" className="product" alt="shoe" />
+            <p className="trending">Trending</p>
+          </div>
+          <button
+            type="button"
+            className={
+              isEight
+                ? "btn3 btn btn-primary sign sidemenu"
+                : "btn3 btn sidemenu sign"
+            }
+            onClick={handleClick7}
+          >
+            <AiOutlineUserAdd className="menuicon" />
+            Login / Signup
+          </button>
+        </div>
+      </div>
+    </div>
+    <div className="col-10"><Signup /></div></div>
+    </div>);
 }
 
 export default Login;
